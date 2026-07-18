b24ea3d440198b779d30333df608a4cb0b2c78a0
Thiago Motta <104937611+thiagomassensini@users.noreply.github.com>
2026-07-18 06:57:33 -0300

    Import Cp carry depth theorem

--- CPFormal.lean
+++ CPFormal.lean
@@ -8,6 +8,7 @@ import CPFormal.Carry.C2WeightedReindex
 import CPFormal.Carry.C2AlignedBox
 import CPFormal.Carry.CpBalancedResidue
 import CPFormal.Carry.CpGlobalIncidence
+import CPFormal.Carry.CpDepth
 import CPFormal.Genuine.BalancedOffsets
 import CPFormal.Genuine.FiniteCancellation
 import CPFormal.Genuine.C2
