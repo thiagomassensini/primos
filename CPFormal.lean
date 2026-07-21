7a0597d5c357b173b2bf5860e3ed580835d424e5
Thiago Motta <104937611+thiagomassensini@users.noreply.github.com>
2026-07-21 17:53:55 -0300

    Import finite weighted TFVD telescoping

--- CPFormal.lean
+++ CPFormal.lean
@@ -70,6 +70,7 @@ import CPFormal.Analytic.CpCarryL2UnilateralShift
 import CPFormal.Analytic.CpCarryWeightedVerticalBracketTrace
 import CPFormal.Analytic.CpCarryWeightedVerticalReturn
 import CPFormal.Analytic.CpCarryWeightedVerticalTfvd
+import CPFormal.Analytic.CpCarryWeightedVerticalTfvdFinite
 import CPFormal.Analytic.CpFiniteGenuineAngularGreenBudget
 import CPFormal.Analytic.CpFiniteGenuineOneSidedGreenBudget
 import CPFormal.Analytic.CpGenuineSecondDifferenceIdentity
