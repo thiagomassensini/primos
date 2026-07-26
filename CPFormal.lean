25b603381d3a3b0fd8a1be7cc147dca8bd9dcaf5
Thiago Motta <104937611+thiagomassensini@users.noreply.github.com>
2026-07-25 21:05:53 -0300

    build: include native carry log-jet generator

--- CPFormal.lean
+++ CPFormal.lean
@@ -97,6 +97,7 @@ import CPFormal.Analytic.CpNativeGpreFiniteGluedGreenDefect
 import CPFormal.Analytic.CpNativeGpreHilbertGluedGreenDefect
 import CPFormal.Analytic.CpNativeCarrySymmetricBoundaryCondition
 import CPFormal.Analytic.CpNativeCarryLogPhaseOrbit
+import CPFormal.Analytic.CpNativeCarryLogJetGenerator
 import CPFormal.Analytic.CpCarryVerticalBracketSymmetryNoGo
 import CPFormal.Analytic.CpGenuineGprePrimeMomentCrosswalk
 import CPFormal.Analytic.CpGenuineGpreLogJetGreenBulkReadout
